c1a2cad11ef9b9b94fe1c5cb25afc3d8ecbe06ea
Brohmarr <panza.thiagoj@gmail.com>
2024-05-16 22:16:22 -0300

    Add logic to retrieve the input data

--- inventory.py
+++ inventory.py
@@ -3,6 +3,17 @@ import csv
 
 class Inventory:
     
-    # TODO: Implement this!
     def __init__(self):
-        pass    
+        self.price_per_item = {}
+
+        # Opening the CSV file to get the input data.
+        with open('items.txt', mode = 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            
+            # Copying the data received into a dictionary for fast lookups.
+            for row in csv_reader:
+                self.price_per_item[row["name"]] = row["price"]
+    
+    # Retrieving the data received.
+    def get_inventory(self):
+        return self.price_per_item
