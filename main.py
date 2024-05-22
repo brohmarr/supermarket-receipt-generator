965b52a86e2826a5fdb8323656adcb3d82b526be
Brohmarr <panza.thiagoj@gmail.com>
2024-05-22 12:37:41 -0300

    Add comments

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from supermarket import Supermarket
 def main():
     supermarket = Supermarket("Fresh Fruits")
     
+    # Generating the receipt (a list of strings).
     receipt = supermarket.build_receipt()
+    
+    # Creating the receipt text file and writing the receipt data in it.
     first_line = True
     with open("receipt.txt", mode = "w") as receipt_file:
         for line in receipt:
@@ -18,7 +21,7 @@ def main():
                 first_line = False
 
                 continue
-            
+
             receipt_file.write("\n" + line)
 
 
